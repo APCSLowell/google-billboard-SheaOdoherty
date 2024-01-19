4cd01d5994056f89017ec3e266a8c4eb049f986b
SheaOdoherty <143014763+SheaOdoherty@users.noreply.github.com>
2024-01-19 10:10:04 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,28 @@
-
-
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
-	}
-	
-
-	//Finish this function
-	public boolean isPrime(double d){
-	}
-
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public void setup()  {            
+ String digits = e.substring(2,12);
+ 
+ for(int i =3; i <e.length()-10; i++){
+   String substring = e.substring(i, i +10);
+   double potential  =Double.parseDouble(substring);
+  if(isPrime(potential)){
+    System.out.println(potential);
+    break;
+  }
+ }
 }
-
+ 
+ public static boolean isPrime(double num){
+  boolean result = true;
+  if(num <=1){
+    result = false;
+  }else{
+  for(int i = 2; i<= Math.sqrt(num); i ++){
+    if(num % i == 0){
+      result = false;
+      break;
+    }
+  }
+}
+  return result;
+}
